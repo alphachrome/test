45831e501575fcaa89a7ec548ffac1816d2b9763
roychan <roychan6@gmail.com>
2018-03-25 21:22:49 -0700

    sysclass.py

--- scope/sysclass.py
+++ scope/sysclass.py
@@ -1,11 +1,14 @@
 #!/usr/local/conda2/bin/python
+#
+# Example using browser: localhost:9000/sys/class/power_supply/BAT0/capacity
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/sys/class')
-def sysvar_read():
-    with open('/sys/class/'+request.args.get('var'),'r') as fd:
+@app.route('/sys/<path:filename>')
+def sysvar_read(filename):
+    with open('/sys/'+filename,'r') as fd:
         return fd.readline().rstrip()
 
 app.run(debug=True, port=9000)
