6121db16d750bcf3a2ae2cf91a20215181d63c80
Cr Test <cros4roy@gmail.com>
2018-03-27 14:55:52 -0700

    install.sh

---
+++ dash/install.sh
@@ -0,0 +1,4 @@
+/usr/local/conda2/bin/pip install dash
+/usr/local/conda2/bin/pip install dash_renderer
+/usr/local/conda2/bin/pip install dash_html_components
+/usr/local/conda2/bin/pip install dash_core_components
